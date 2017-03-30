a6942b6b70cbe00bb78d743c8711f6e48b81734e
Vinícius Pimentel <vinipcouto@gmail.com>
2017-03-30 15:59:07 -0300

    Added key debugger

--- Assets/Scripts/KeyDetector.cs
+++ Assets/Scripts/KeyDetector.cs
@@ -1,51 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using UnityEngine.UI;
 
-// Detects which buttons are pressed and 
-//	lights up the corresponding arrow
 public class KeyDetector : MonoBehaviour 
 {
-	public Image upLeft, up, upRight, right, downRight, down, downLeft, left;
+	public Text keyDebugger;
+
+	private void Awake()
+	{
+		keyDebugger.text = "";
+	}
 
 	private void Update()
 	{
-		DetectPressedKeyOrButton();
-	}
-
-	public void DetectPressedKeyOrButton()
-	{
-		ResetColors();
-
-		if (Input.GetKey(KeyCode.JoystickButton0))
-			downLeft.color = Color.red;
-
-		if (Input.GetKey(KeyCode.JoystickButton1))
-			upRight.color = Color.red;
-
-		if (Input.GetKey(KeyCode.JoystickButton2))
-			upLeft.color = Color.red;
-
-		if (Input.GetKey(KeyCode.JoystickButton3))
-			downRight.color = Color.red;
-		
-		if (Input.GetAxis("Horizontal") > 0f)
-			right.color = Color.red;
-		
-		if (Input.GetAxis("Horizontal") < 0f)
-			left.color = Color.red;
-		
-		if (Input.GetAxis("Vertical") > 0f)
-			up.color = Color.red;
-
-		if (Input.GetAxis("Vertical") < 0f)
-			down.color = Color.red;
-	}
-
-	private void ResetColors()
-	{
-		upLeft.color = up.color = upRight.color = right.color = downRight.color = down.color = downLeft.color = left.color = Color.white;
+		foreach(KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+		{
+			if(Input.GetKeyDown(vKey))
+			{
+				keyDebugger.text = string.Concat(keyDebugger.text, "\n", vKey.ToString());
+			}
+		}
 	}
 }
